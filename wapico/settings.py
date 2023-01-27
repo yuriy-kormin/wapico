bf6d4cec173f245176f132959884476f8d888726
Yuriy Kormin <yuriy.kormin@gmail.com>
2023-01-27 14:13:45 +0400

    modify setting to use dj_database_url. config

--- wapico/settings.py
+++ wapico/settings.py
@@ -85,12 +85,16 @@ WSGI_APPLICATION = 'wapico.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
+#
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config()
 }
 
 

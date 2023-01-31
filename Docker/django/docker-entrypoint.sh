14984d13f65a281b4bb790b24d2a370114c60a16
Yuriy Kormin <yuriy.kormin@gmail.com>
2023-01-31 12:16:17 +0400

    docker

---
+++ Docker/django/docker-entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#python manage.py initadmin
+######################################
+#### planning to write initadmin  ####
+######################################
+
+
+# Apply database migrations
+echo "Apply database migrations"
+python manage.py migrate --noinput
+
+#collect static
+python manage.py collectstatic --noinput
+
+# Start server
+echo "Starting jango app"
+gunicorn wapico.wsgi:application --bind 0.0.0.0:8000

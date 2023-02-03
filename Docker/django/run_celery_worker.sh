992b3e678b299a6a66312ba79e36b406727dc7bd
Yuriy Kormin <yuriy.kormin@gmail.com>
2023-02-03 14:12:27 +0400

    optimizing docker-compose to reuse containers

--- Docker/django/run_celery_worker.sh
+++ Docker/django/run_celery_worker.sh
@@ -11,9 +11,4 @@ pip3 install poetry
 poetry config virtualenvs.create false
 poetry install
 
-# Apply database migrations
-echo "Apply database migrations"
-python manage.py migrate --noinput
-
-#collect static
-python manage.py collectstatic --noinput
+celery -A wapico.celery worker -l info

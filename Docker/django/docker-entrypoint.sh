692ed07b4c1a3bc724ce2a76db461f0f38b3e855
Yuriy Kormin <yuriy.kormin@gmail.com>
2023-02-08 08:34:22 +0400

    move migrate option to run django script

--- Docker/django/docker-entrypoint.sh
+++ Docker/django/docker-entrypoint.sh
@@ -14,7 +14,6 @@ poetry config virtualenvs.create false
 poetry install
 # Apply database migrations
 echo "Apply database migrations"
-python manage.py migrate --noinput
 python manage.py loaddata data.json
 #collect static
 python manage.py collectstatic --noinput
--- Docker/django/run_django.sh
+++ Docker/django/run_django.sh
@@ -4,4 +4,5 @@
 cd /app
 
 #run gunicorn
+python manage.py migrate --noinput
 gunicorn wapico.wsgi:application --bind 0.0.0.0:8000

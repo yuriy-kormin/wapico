f8e941d499ab548ec1138666ef2b3ed993de36b1
Yuriy Kormin <yuriy.kormin@gmail.com>
2023-03-10 16:59:26 +0400

    modify nav

--- Docker/django/docker-entrypoint.sh
+++ Docker/django/docker-entrypoint.sh
@@ -13,7 +13,5 @@ pip3 install poetry
 poetry config virtualenvs.create false
 poetry install
 # Apply database migrations
-echo "Apply database migrations"
-python manage.py loaddata data.json
 #collect static
 python manage.py collectstatic --noinput

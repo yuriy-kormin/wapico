7348dac7ed46cfd334ab6752fc5a870c8eecac40
Yuriy Kormin <yuriy.kormin@gmail.com>
2023-03-27 15:25:24 +0400

    fix createsuperuser

--- Docker/django/docker-entrypoint.sh
+++ Docker/django/docker-entrypoint.sh
@@ -14,8 +14,9 @@ poetry config virtualenvs.create false
 poetry install
 # Apply database migrations
 #collect static
-python manage.py collectstatic --noinput
-echo "createsuperuser"
+#python manage.py collectstatic --noinput
+
+echo "create django superuser"
 python manage.py createsuperuser --noinput \
       --username $DJANGO_SUPERUSER_USERNAME \
       --email $DJANGO_SUPERUSER_EMAIL

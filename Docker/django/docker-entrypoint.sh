d5d990ecb8e499476a63fcb45daadb1fd8aa6bec
Yuriy Kormin <yuriy.kormin@gmail.com>
2023-03-27 15:28:28 +0400

    fix 1

--- Docker/django/docker-entrypoint.sh
+++ Docker/django/docker-entrypoint.sh
@@ -9,6 +9,7 @@
 
 mkdir -p /app/wapico/static
 cd /app
+python3 -m pip install --upgrade pip
 pip3 install poetry
 poetry config virtualenvs.create false
 poetry install
@@ -16,7 +17,7 @@ poetry install
 #collect static
 #python manage.py collectstatic --noinput
 
-echo "create django superuser"
-python manage.py createsuperuser --noinput \
-      --username $DJANGO_SUPERUSER_USERNAME \
-      --email $DJANGO_SUPERUSER_EMAIL
+#echo "create django superuser"
+#python manage.py createsuperuser --noinput \
+#      --username $DJANGO_SUPERUSER_USERNAME \
+#      --email $DJANGO_SUPERUSER_EMAIL

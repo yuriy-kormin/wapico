f025ae672cadf5f9adff7b8004d5c0e9e97374fe
Yuriy Kormin <yuriy.kormin@gmail.com>
2023-03-28 10:10:33 +0400

    register is_list filter and apply it to template output

--- wapico/task/templatetags/extras.py
+++ wapico/task/templatetags/extras.py
@@ -8,3 +8,8 @@ register = template.Library()
 @register.filter
 def pretty_json(value):
     return json.dumps(value, indent=4)
+
+
+@register.filter
+def is_list(value):
+    return isinstance(value, list)

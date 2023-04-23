e636d2910aa177587e2cc6f5c96e46b38b2d37f1
yuriy.kormin <yuriy.kormin@gmail.com>
2023-04-23 10:42:35 +0400

    add flower header

--- Docker/django/run_flower.sh
+++ Docker/django/run_flower.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd /app/
-celery -A wapico.celery flower --port=5566 --url-prefix=flower
+celery -A wapico.celery flower --port=5566 --url-prefix=flower --inspect-options=footer_template:templates/flower_header.html

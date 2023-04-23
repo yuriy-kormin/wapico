63c7339675138301365b4cc629374eecc80ecd91
yuriy.kormin <yuriy.kormin@gmail.com>
2023-04-23 11:03:19 +0400

    update custom templates to flower

--- Docker/django/run_flower.sh
+++ Docker/django/run_flower.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd /app/
-celery -A wapico.celery flower --port=5566 --url-prefix=flower --inspect-options=footer_template:templates/flower_header.html
+celery -A wapico.celery flower --port=5566 --url-prefix=flower --inspect-options=custom_templates:templates/flower_header.html
